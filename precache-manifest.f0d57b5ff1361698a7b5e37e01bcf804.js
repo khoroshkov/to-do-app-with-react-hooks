7600722aa504a1cb73f6ad9732c786f1dcc6baa3
Nick_Khoroshkov <nickolaykhoroshkov@gmail.com>
2020-04-08 12:53:16 +0300

    Updates

---
+++ precache-manifest.f0d57b5ff1361698a7b5e37e01bcf804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42966d6664cc8b9ce0de",
+    "url": "/to-do-app-with-react-hooks/static/js/runtime~main.85dec996.js"
+  },
+  {
+    "revision": "c9a7ff6a479c14e7736b",
+    "url": "/to-do-app-with-react-hooks/static/js/main.0f4ec7e8.chunk.js"
+  },
+  {
+    "revision": "0c53888cf63925c6e19b",
+    "url": "/to-do-app-with-react-hooks/static/js/2.6a256418.chunk.js"
+  },
+  {
+    "revision": "c9a7ff6a479c14e7736b",
+    "url": "/to-do-app-with-react-hooks/static/css/main.d363f206.chunk.css"
+  },
+  {
+    "revision": "0c53888cf63925c6e19b",
+    "url": "/to-do-app-with-react-hooks/static/css/2.0e18603d.chunk.css"
+  },
+  {
+    "revision": "d9b3d8e4e9d409aba89f38d50519f08d",
+    "url": "/to-do-app-with-react-hooks/index.html"
+  }
+];
